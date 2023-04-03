d79bfcd04fc1a6ce0e7bcb859ff2f748c8ff91bb
angelpulido96 <angelhpulido@gmail.com>
2023-04-03 11:52:11 -0600

    deleted logs

--- components/chat/service.js
+++ components/chat/service.js
@@ -16,11 +16,9 @@ const createChat = async (partakers) => {
   try {
 
     const users = await User.find({ _id: { $in: partakers } })
-    console.log("🚀 ~ createChat ~ users:", users)
 
     const createdChat = new Chat(users)
     await createdChat.save()
-    console.log("🚀 ~ createChat ~ createdChat:", createdChat)
 
     response.created = true
   } catch (error) {
